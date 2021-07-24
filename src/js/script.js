468c64203b2ae833c91b00f6c3577e7aab9fd401
Alexey <kirilin_alesha@mail.ru>
2021-07-25 02:53:09 +0400

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,61 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    close = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+    lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1400) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+    
+    $("a[href=#about]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+    $("a[href=#portfolio]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+
+    new WOW().init();
+    
+});
+
+
+
+
+//scroll and pageup
+// $(window).scroll(function () {
+//     if ($(this).scrollTop() > 1600) {
+//         $('.pageup').fadeIn();
+//     } else {
+//         $('.pageup').fadeOut();
+//     };
+    
+// });
